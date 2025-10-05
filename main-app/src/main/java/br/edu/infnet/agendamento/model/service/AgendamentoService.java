11e342a853dd13d57968fa3a66832bc00dd23f6c
Maria Clara Mussi <mariamussis@outlook.com>
2025-10-05 10:54:40 -0300

    Fix: remove unnecessary Feriado DTO & add agendamento validation

--- main-app/src/main/java/br/edu/infnet/agendamento/model/service/AgendamentoService.java
+++ main-app/src/main/java/br/edu/infnet/agendamento/model/service/AgendamentoService.java
@@ -1,6 +1,7 @@
 package br.edu.infnet.agendamento.model.service;
 
 
+import br.edu.infnet.agendamento.clients.BrasilApiFeignClients;
 import br.edu.infnet.agendamento.exceptions.AgendamentoInvalidoException;
 import br.edu.infnet.agendamento.exceptions.AgendamentoNaoExisteException;
 import br.edu.infnet.agendamento.model.domain.Agendamento;
@@ -8,6 +9,8 @@ import br.edu.infnet.agendamento.model.repository.AgendamentoRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,19 +18,26 @@ import java.util.Optional;
 public class AgendamentoService implements CrudService<Agendamento, Integer> {
 
     private final AgendamentoRepository agendamentoRepository;
+    private final BrasilApiFeignClients brasilApiFeignClients;
 
-    public AgendamentoService(AgendamentoRepository agendamentoRepository) {
+    public AgendamentoService(AgendamentoRepository agendamentoRepository, BrasilApiFeignClients brasilApiFeignClients) {
         this.agendamentoRepository = agendamentoRepository;
+        this.brasilApiFeignClients = brasilApiFeignClients;
     }
 
     private void validar(Agendamento agendamento) {
         if(agendamento == null) {
             throw new IllegalArgumentException("O agendamento não pode ser vazio");
         }
+
         if(agendamento.getPaciente() == null) {
             throw new AgendamentoInvalidoException("O paciente é obrigatório");
         } else if (agendamento.getMedico() == null) {
             throw new AgendamentoInvalidoException("O médico é obrigatório");
+        }
+
+        if(verificarFeriado(agendamento.getData())) {
+            throw new AgendamentoInvalidoException("O agendamento nao pode ocorrer no feriado");
         }
     }
 
@@ -81,5 +91,12 @@ public class AgendamentoService implements CrudService<Agendamento, Integer> {
         Optional<Agendamento> agendamento = agendamentoRepository.findByData(data);
 
         return agendamento.isPresent();
+    }
+
+    private Boolean verificarFeriado(String data) {
+        String[] str = data.split("/");
+        List<BrasilApiFeignClients.Feriado> feriados = brasilApiFeignClients.obterFeriadosPorAno(str[2]);
+
+        return feriados.stream().anyMatch(f -> f.getDate().isEqual(LocalDate.parse(data, DateTimeFormatter.ofPattern("dd/MM/yyyy"))));
     }
 }
